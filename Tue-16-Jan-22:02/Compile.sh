512197f89bae1533c64010bbbbec851c023ea1d5
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-16 22:17:52 -0700

    Add files via upload

---
+++ Tue-16-Jan-22:02/Compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mods_html="Mods.html"
+insert_position=$(grep -n '<a href="index.html" class="movie-button">Home</a>' $mods_html | cut -d: -f1)
+sed -i '/<a href="Mods\//d' $mods_html
+for mod_folder in Mods/*; do
+    if [ -d "$mod_folder" ]; then
+        # Check if the subdirectory contains index.html
+        if [ -e "$mod_folder/index.html" ]; then
+            # Get the mod name from the folder name
+            mod_name=$(basename "$mod_folder")
+            sed -i "${insert_position}a \ \ \ \ <a href=\"${mod_folder}/index.html\" class=\"movie-button\">${mod_name}</a>" $mods_html
+        else
+            sed -i "/<a href=\"Mods\/${mod_name}\/index.html\"/d" $mods_html
+        fi
+    fi
+done
+
+echo "Script executed successfully!"
---
+++ Tue-16-Jan-22:02/README.md
@@ -0,0 +1,2 @@
+# Mods
+This is the folder is that holds the testing for mods
