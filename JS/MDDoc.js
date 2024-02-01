f3290703b9f03b671a1d5561ddc91fd18b1b898d
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-31 21:34:54 -0700

    Makes md code load autoly from the mark down raw file

---
+++ JS/MDDoc.js
@@ -0,0 +1,6 @@
+  fetch('https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/Repo-docs/README.md')
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('md-block').innerHTML = data;
+    })
+    .catch(error => console.error('Erorr getting the mark down file:', error));
