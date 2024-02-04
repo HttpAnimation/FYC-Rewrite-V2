6d9b017209b94a9441052c6971321268c1a59e2e
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-03 21:44:08 -0700

    Adds SeedersAtTheTime

--- Scripts/JS/Windows-Apps.js
+++ Scripts/JS/Windows-Apps.js
@@ -19,56 +19,56 @@ function handleSourceButtonClick(sourceUrl) {
 }
 
 fetch('../Configs/Replers/Windows-Apps-Repo.json')
-.then(response => response.json())
-.then(data => {
-    const repositories = data[0]['Repositories'];
-    const contentElement = document.getElementById('content');
+    .then(data => {
+        const repositories = data[0]['Repositories'];
+        const contentElement = document.getElementById('content');
 
-    const fetchRepoData = async (repoURL) => {
-        try {
-            const response = await fetch(repoURL);
-            const repoData = await response.json();
-    
-            const movies = repoData['Movies'] || repoData[0]['Movies'];
-    
-            if (movies) {
-                Object.keys(movies).forEach(key => {
-                    const movie = movies[key];
-                    const movieElement = document.createElement('div');
-                    movieElement.innerHTML = `
-                        <h1>${movie['Name']}</h1>
-                        <img src="${movie['Icon']}" alt="Movie Icon">
-                        <button class="movie-button" onclick="handleSourceButtonClick('${movie['Source']}')">Source</button>
-                        <h6><pre><code>${movie['Description']}</code</pre</h6>
-                        <a href="${movie['.Torrent']}" target="_blank" class="movie-button">Download Torrent</a>
-                        <a href="${movie['MagnetUrl']}" class="movie-button">Magnet Link</a>
-                    `;
-                    if (movie['StreamURL']) {
-                        const streamButton = document.createElement('a');
-                        streamButton.href = movie['StreamURL'];
-                        streamButton.classList.add('movie-button');
-                        streamButton.textContent = 'Open Stream';
-                        movieElement.appendChild(streamButton);
-                    }
-                    contentElement.appendChild(movieElement);
-                });
-            } else {
-                console.error(`Error fetching repo data from ${repoURL}: Invalid data format.`);
+        const fetchRepoData = async (repoURL) => {
+            try {
+                const response = await fetch(repoURL);
+                const repoData = await response.json();
+        
+                const movies = repoData['Movies'] || repoData[0]['Movies'];
+        
+                if (movies) {
+                    Object.keys(movies).forEach(key => {
+                        const movie = movies[key];
+                        const movieElement = document.createElement('div');
+                        movieElement.innerHTML = `
+                            <h1>${movie['Name']}</h1>
+                            <img src="${movie['Icon']}" alt="Movie Icon">
+                            <button class="movie-button" onclick="handleSourceButtonClick('${movie['Source']}')">Source</button>
+                            <h4>${movie['SeedersAtTime']}</h4>
+                            <h6>${movie['Description']}</h6>
+                            <a href="${movie['.Torrent']}" target="_blank" class="movie-button">Download Torrent</a>
+                            <a href="${movie['MagnetUrl']}" class="movie-button">Magnet Link</a>
+                            `;
+                        if (movie['StreamURL']) {
+                            const streamButton = document.createElement('a');
+                            streamButton.href = movie['StreamURL'];
+                            streamButton.classList.add('movie-button');
+                            streamButton.textContent = 'Open Stream';
+                            movieElement.appendChild(streamButton);
+                        }
+                        contentElement.appendChild(movieElement);
+                    });
+                } else {
+                    console.error(`Error fetching repo data from ${repoURL}: Invalid data format.`);
+                    showError(`Error fetching repo data from ${repoURL}.`);
+                }
+            } catch (error) {
+                console.error(`Error fetching repo data from ${repoURL}:`, error);
                 showError(`Error fetching repo data from ${repoURL}.`);
             }
-        } catch (error) {
-            console.error(`Error fetching repo data from ${repoURL}:`, error);
-            showError(`Error fetching repo data from ${repoURL}.`);
-        }
-    };
+        };
 
-    repositories.forEach(repoURL => {
-        const correctedRepoURL = repoURL.trim();
-        fetchRepoData(correctedRepoURL);
+        repositories.forEach(repoURL => {
+            const correctedRepoURL = repoURL.trim();
+            fetchRepoData(correctedRepoURL);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching repo list you might need cross-origin:', error);
     });
-})
-.catch(error => {
-    console.error('Error fetching repo list you might need cross-origin:', error);
-});
 
 console.log('Windows-Apps.js | Loaded');
