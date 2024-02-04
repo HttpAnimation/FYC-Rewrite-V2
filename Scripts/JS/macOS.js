8097020b8fe1a973162b4f8dd283ec06c113be45
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-03 22:32:53 -0700

    adds new repo system

--- Scripts/JS/macOS.js
+++ Scripts/JS/macOS.js
@@ -5,12 +5,23 @@ function showError(message) {
     document.body.insertBefore(errorMessage, document.getElementById('sidebar'));
 }
 
-function handleButtonClick(buttonName, link) {
-    console.log(`Button clicked: ${buttonName}`);
-    if (link) {
-        console.log(`Redirecting to: ${link}`);
-        window.location.href = link;
+function toggleDescriptionVisibility(movieId) {
+    const descriptionElement = document.getElementById(`description_${movieId}`);
+    if (descriptionElement.style.display === 'none' || !descriptionElement.style.display) {
+        descriptionElement.style.display = 'block';
+    } else {
+        descriptionElement.style.display = 'none';
     }
+}
+
+function handleButtonClick(event, movieName, sourceUrl, movieId) {
+    event.preventDefault(); // Prevent the default behavior of the button click
+    console.log(`Button clicked: ${movieName}`);
+    if (sourceUrl) {
+        console.log(`Opening source URL: ${sourceUrl}`);
+        window.open(sourceUrl, '_blank');
+    }
+    toggleDescriptionVisibility(movieId);
 }
 
 function handleSourceButtonClick(sourceUrl) {
@@ -35,10 +46,14 @@ fetch('../Configs/Replers/macOS-Repo.json')
                     Object.keys(movies).forEach(key => {
                         const movie = movies[key];
                         const movieElement = document.createElement('div');
+                        const movieId = `movie_${key}`;
+                        movieElement.id = movieId;
                         movieElement.innerHTML = `
                             <h1>${movie['Name']}</h1>
                             <img src="${movie['Icon']}" alt="Movie Icon">
-                            <button class="movie-button" onclick="handleSourceButtonClick('${movie['Source']}')">Source</button>
+                            <h4>${movie['SeedersAtTime']}</h4>
+                            <h6 id="description_${movieId}" style="display: none;">${movie['Description']}</h6>
+                            <button class="movie-button" onclick="handleButtonClick(event, '${movie['Name']}', '${movie['Source']}', '${movieId}')">Toggle Description</button>
                             <a href="${movie['.Torrent']}" target="_blank" class="movie-button">Download Torrent</a>
                             <a href="${movie['MagnetUrl']}" class="movie-button">Magnet Link</a>
                         `;
@@ -68,6 +83,7 @@ fetch('../Configs/Replers/macOS-Repo.json')
     })
     .catch(error => {
         console.error('Error fetching repo list you might need cross-origin:', error);
+        console.log('Error response:', error.response);
     });
 
-console.log('MacOS.js | Loaded');
+console.log('macOS.js | Loaded');
