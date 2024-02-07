3ed4f12482463ee702e02c9d5b1132e62b5bd40a
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-07 08:41:13 -0700

    Create git.bash

---
+++ git.bash
@@ -0,0 +1,24 @@
+mkdir FYC-Rewrite-V2
+cd FYC-Rewrite-V2
+git clone -b Stable https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Stable
+git clone -b main https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 main
+git clone -b gh-pages https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 gh-pages
+git clone -b Mod-Docs https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Mod-Docs
+git clone -b NodeJSSever https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 NodeJSSever
+git clone -b Repo https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Repo
+git clone -b Testing https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Testing
+git clone -b V2 https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 V2
+git clone -b Version-1 https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Version-1
+git clone -b VersionTemplate https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 VersionTemplate
+git clone -b Win-installer https://github.com/HttpAnimaion/FYC-Rewrite-V2
+mv FYC-Rewrite-V2 Win-installer
