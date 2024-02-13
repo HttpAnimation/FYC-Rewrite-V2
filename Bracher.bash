bf1fdb0948bd691167bc5c15311ba0f4e2130840
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-02-13 12:08:13 -0500

    Create Bracher.bash

---
+++ Bracher.bash
@@ -0,0 +1,22 @@
+mkdir FYC-Rewrite\ V2
+cd FYC-Rewrite\ V2
+git clone -b main https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 main
+git clone -b Mod-Docs https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Mod-Docs
+git clone -b NodeJSSever https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 NodeJSSever
+git clone -b Repo https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Repo
+git clone -b Repo-Docs https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Repo-Docs
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Testing https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Testing
+git clone -b V2 https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 V2
+git clone -b Version-1 https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Version-1
+git clone -b VersionTemplate https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 VersionTemplate
