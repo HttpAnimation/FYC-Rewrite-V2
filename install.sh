d46b5835fbcd2586208c2ffbc90f17df41ac061c
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-14 11:48:26 -0500

    removes read answer

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ rm -r Templates
 rm Configs/Replers/README.md
 rm Mods/README.md
 
-read answer
 
 wget "https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/NodeJSSever/InstallWget.sh" && chmod +x InstallWget.sh && ./InstallWget.sh
 
