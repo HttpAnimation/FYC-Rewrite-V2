9d4dcd917a59c6fa878260f9a6ee478fb42fd110
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-14 11:48:15 -0500

    removes echo "Would you like to install the node.js server npm will need to be installed for this? (y/n)"

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ rm -r Templates
 rm Configs/Replers/README.md
 rm Mods/README.md
 
-echo "Would you like to install the node.js server npm will need to be installed for this? (y/n)"
 read answer
 
 wget "https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/NodeJSSever/InstallWget.sh" && chmod +x InstallWget.sh && ./InstallWget.sh
