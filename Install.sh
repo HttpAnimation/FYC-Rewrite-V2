0251fddd41b75db3483f7beeee965bf3e0523372
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-17 19:40:54 -0700

    downloads package-lock.json

--- Install.sh
+++ Install.sh
@@ -8,3 +8,4 @@ rm Mods/README.md
 
 # Nodejs
 wget https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/NodeJSSever/Run.sh
+wget https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/NodeJSSever/package-lock.json
