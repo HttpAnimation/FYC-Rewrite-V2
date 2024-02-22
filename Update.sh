a742e4a6cb6e99e72c4a792b8d8b5c8fd602f442
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-21 19:02:29 -0700

    adds install commnad

--- Update.sh
+++ Update.sh
@@ -4,3 +4,4 @@ cd Configs
 mv Replers ../../../Replers
 cd ../../../
 rm -rf FYC 
+wget https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/main/Install.sh && chmod +x Install.sh && bash Install.sh
