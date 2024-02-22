a2c2c20fc5582d06af3e64e700a7fd360266a3c7
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-21 19:10:02 -0700

    adds a url

--- Install.sh
+++ Install.sh
@@ -39,7 +39,7 @@ cd Update
 wget https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/main/Update.sh
 chmod +x Update.sh
 cd ../
-wget
+wget https://raw.githubusercontent.com/HttpAnimation/FYC-Rewrite-V2/main/Update.sh
 
 cd ../
 rm $0
