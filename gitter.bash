4a73256d32f7232d2964a55aa97016fe83833147
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-08 14:17:19 -0500

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -3,24 +3,30 @@ mkdir FYC-Rewrite-V2
 cd FYC-Rewrite-V2
 git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
 mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
-git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
-mv FYC-Rewrite-V2 Stable
+git clone -b Mode-Docs https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Mode-Docs
+git clone -b NodeJSSever https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 NodeJSSever
+git clone -b Repo https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Repo
+git clone -b Repo-docs https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Repo-docs
+git clone -b Testing https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Testing
+git clone -b V2 https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 V2
+git clone -b Version-1 https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Version-1
+git clone -b VersionTemplate https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 VersionTemplate
+git clone -b Win-installer https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Win-installer
+git clone -b blank https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 blank
+git clone -b gh-pages https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 gh-pages
+git clone -b gitter https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 gitter
 git clone -b main https://github.com/HttpAnimation/FYC-Rewrite-V2.git
 mv FYC-Rewrite-V2 main
 git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
