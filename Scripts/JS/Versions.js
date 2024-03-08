933df149c1337fe4f549e52585202a4cdbc671e2
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-08 14:53:39 -0500

    br after each

--- Scripts/JS/Versions.js
+++ Scripts/JS/Versions.js
@@ -39,6 +39,7 @@ fetch('Configs/Replers/Versions.json')
                             <h1>${movie['Name']}</h1>
                             <a href="${movie['.Torrent']}" target="_blank" class="movie-button">Download Tar.gz / Linux | macOS</a>
                             <a href="${movie['MagnetUrl']}" target="_blank" class="movie-button">Download Zip / Windows</a>
+                            <br>
                         `;
                         if (movie['HasStreamURL'] === true) {
                             const streamButton = document.createElement('a');
