922eb4db8e9b8efc42a6f06fb7fb94e0008c0c18
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-08 14:13:45 -0500

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,5 +1,31 @@
 # Put gitter here
 mkdir FYC-Rewrite-V2
 cd FYC-Rewrite-V2
-git clone -b stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
+mv FYC-Rewrite-V2 Stable
+git clone -b Stable https://github.com/HttpAnimation/FYC-Rewrite-V2.git
 mv FYC-Rewrite-V2 Stable
